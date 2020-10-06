e66ae313cdd9d418deb97ce2650a2a2bca88c69c
Seo Hyun Park <morecreativa@naver.com>
2020-10-06 21:07:45 +0900

    Update BOJ.cpp

--- TestCpp/BOJ.cpp
+++ TestCpp/BOJ.cpp
@@ -24,6 +24,7 @@ using namespace std;
 #define LL long long
 
 // BOJ 5052
+#if 0
 int main(){
     int N;
     scanf("%d",&N);
@@ -47,8 +48,10 @@ int main(){
     }
     return 0;
 }
+#endif 
 
-// Programmers
+// Programmers Find Prime numbers
+#if 0
 bool isPrimeNumber(int x){
     if(x<=1) return 0;
     int answer=0;
@@ -78,3 +81,24 @@ int solution(string numbers) {
     }while(next_permutation(numbers.begin(),numbers.end()));
     return answer;
 }//next permutation don't show all permutation
+#endif
+
+// Programmers Carpet
+#if 1
+bool compare(const int &a , const int &b){
+    return a>b;
+}
+
+vector<int> solution(int brown, int yellow) {
+    vector<int> answer;
+    for(int i=1;i*i<=yellow;++i){
+        if(yellow%i==0 && (i+yellow/i)*2+4==brown){
+            answer.push_back(i+2);
+            answer.push_back(yellow/i+2);
+            sort(answer.begin(),answer.end(),compare);
+            break;
+        }
+    }
+    return answer;
+}
+#endif
